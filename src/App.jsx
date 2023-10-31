86caa5fc789c29306c963f192afb269ca764657c
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-10-31 18:16:21 +0100

    UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import { TasksList } from "./components/TasksList";
 
+import { useState, useEffect } from "react";
+
 function App() {
+	const [counter, setCounter] = useState(0);
+	useEffect(() => {
+		console.log("Counter: "+counter);
+	}, [counter]);
 	return (
 		<div>
-			<TasksList />
+			{/* <TasksList /> */}
+
+			<button onClick={() => setCounter(counter + 1)}>{counter}</button>
 		</div>
 	);
 }

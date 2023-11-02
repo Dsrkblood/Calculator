16da5a1bc5c187ddb3e0888f05c4207294087f20
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-11-02 22:25:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import { useState, useEffect } from "react";
 function App() {
 	const [counter, setCounter] = useState(0);
 	useEffect(() => {
-		console.log("Counter: "+counter);
+		console.log("Counter: " + counter);
 	}, [counter]);
 	return (
 		<div>
-			{/* <TasksList /> */}
+			<TasksList />
 
 			<button onClick={() => setCounter(counter + 1)}>{counter}</button>
 		</div>

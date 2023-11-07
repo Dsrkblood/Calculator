d754b2d7471cf0fe34c448eebdd2e1d37292b91c
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-11-07 12:07:50 +0100

    Still learning

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-class App extends React.Component {
-	state = {
-		counter: 0,
-	};
-	render() {
-		return (
-			<div>
-				<h1>Hello!</h1>
-			</div>
-		);
-	}
-}
+const header = <h1 className='title'>Witaj na stronie!</h1>;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const classBig = 'big'
+
+const handleClick = () => alert('klik')
+const main = (
+	<div>
+		<h1 onClick={handleClick}className='red'>Pierwszy artykuł</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dicta
+			obcaecati sit ut cupiditate veritatis possimus qui. Architecto, beatae
+			illo! Cum aperiam praesentium repellendus facere eligendi architecto
+			assumenda totam quos!
+		</p>
+	</div>
+);
+const footer = (
+	<footer>
+		<p className={classBig}>Stopka</p>
+	</footer>
+);
+
+const app = [header, main, footer];
+
+ReactDOM.render(app, document.getElementById("root"));

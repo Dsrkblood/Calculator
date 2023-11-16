e9f36e09b9aa8b3ca1c32928809e6e456732d851
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-11-16 19:01:58 +0100

    New Task

--- App.js
+++ App.js
@@ -1,109 +1,60 @@
-const Cash = props => (
-	<div>
-		{props.title}
-		{props.cash <= 0
-			? ""
-			: ((props.cash / props.ratio) * props.price).toFixed(2)}
-	</div>
-);
-
-class ExchangeCounter extends React.Component {
+class Los extends React.Component {
 	state = {
-		amount: "",
-		product: "electricity",
+		rand: null,
+		divination: ["pierwsza wróżba", "druga wróżba", "trzecia wróżba"],
+		value: "",
 	};
 
-	static defaultProps = {
-		currencies: [
-			{
-				id: 0,
-				name: "zloty",
-				ratio: 1,
-				title: "Wartość w złotówkach: ",
-			},
-			{
-				id: 1,
-				name: "dollar",
-				ratio: 4.02,
-				title: "Wartość w dolarach: ",
-			},
-			{
-				id: 2,
-				name: "euro",
-				ratio: 4.38,
-				title: "Wartość w euro: ",
-			},
-			{
-				id: 3,
-				name: "pound",
-				ratio: 5.02,
-				title: "Wartość w funtach: ",
-			},
-		],
-		prices: {
-			electricity: 0.51,
-			gas: 7.38,
-			oranges: 8.49,
-		},
+	handleRand = () => {
+		this.setState({
+			rand: Math.floor(Math.random() * this.state.divination.length),
+		});
 	};
 
-	handleChange = e => {
+	randomMessage() {
+		return <h1>{this.state.divination[this.state.rand]}</h1>;
+	}
+
+	handleDivination = e => {
 		this.setState({
-			amount: e.target.value,
+			value: e.target.value,
 		});
 	};
-	handleSelect = e => {
-		this.setState({
-			product: e.target.value,
-			amount: "",
-		});
+
+	handleAddDivination = () => {
+		// let newArray = this.state.divination.map(el => el);
+		// newArray.push(value);
+		// this.setState({
+		// 	divination: newArray,
+		// 	value: "",
+		// });
+		if (this.state.value === "") return alert("wpisz coś!");
+		else {
+			const divination = [...this.state.divination];
+			divination.push(this.state.value);
+			this.setState({
+				divination,
+				value: "",
+			});
+			alert(`Wróżba dodana. Aktualne wróżby: ${divination}`);
+		}
 	};
-	insertSuffix(select) {
-		if (select === "electricity") return <em> kWh</em>;
-		else if (select === "gas") return <em> litrów</em>;
-		else if (select === "oranges") return <em> kilogramów</em>;
-		else return null;
-	}
-	selectPrice(select) {
-		const price = this.props.prices[select];
-		return price;
-	}
 
 	render() {
-		const { amount, product } = this.state;
-		const price = this.selectPrice(product);
-		const calculators = this.props.currencies.map(currency => (
-			<Cash
-				key={currency.id}
-				ratio={currency.ratio}
-				title={currency.title}
-				cash={amount}
-				price={price}
-			/>
-		));
 		return (
-			<div className='app'>
-				<label>
-					Wybierz produkt:
-					<select value={product} onChange={this.handleSelect}>
-						<option value='electricity'>Prąd</option>
-						<option value='gas'>Benzyna</option>
-						<option value='oranges'>Pomarańcze</option>
-					</select>
-				</label>
+			<>
+				<button onClick={this.handleRand}>Zobacz wróżbę</button>
 				<br />
-				<label>
-					<input
-						type='number'
-						value={this.state.amount}
-						onChange={this.handleChange}
-					/>
-					{this.insertSuffix(this.state.product)}
-				</label>
-				{calculators}
-			</div>
+				<input
+					type='text'
+					value={this.state.value}
+					onChange={this.handleDivination}
+				/>
+				<button onClick={this.handleAddDivination}>Dodaj wróżbę</button>
+				{this.randomMessage()}
+			</>
 		);
 	}
 }
 
-ReactDOM.render(<ExchangeCounter />, document.getElementById("root"));
+ReactDOM.render(<Los />, document.getElementById("root"));

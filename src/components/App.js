5abdb1353a051a5bcc602b37cc32c8189ee120b5
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-11-24 14:06:58 +0100

    Fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
-// Rosnąca baza danych
-const data = [
-	{
-		id: 1,
-		title: "Wiadomość numer 1",
-		body: "Zawartość wiadomości numer 1 ...",
-	},
-	{
-		id: 2,
-		title: "Wiadomość numer 2",
-		body: "Zawartość wiadomości numer 2 ...",
-	},
-];
-// Dodawanie danych do tablicy co 4s
-setInterval(() => {
-	const index = data.length + 1;
-	data.push({
-		id: index,
-		title: `Wiadomośc numer ${index}`,
-		body: `Zawartość wiadomości numer ${index} ...`,
-	});
-}, 4000);
+import UsersList from "./UsersList";
+import ButtonFetchUsers from "./ButtonFetchUsers";
+
+const API = "https://randomuser.me/api/?results=5";
 
 class App extends Component {
 	state = {
-		comments: [...data],
+		users: null,
 	};
 
-	getData = () => {
-		if (this.state.comments.length !== data.length) {
-			this.setState({
-				comments: [...data],
-			});
-		} else {
-			console.log("dane tekie same");
-		}
+	handleDataFetch = () => {
+		fetch(API)
+			.then(response => {
+				if (response.ok) {
+					return response;
+				}
+				throw Error(response.status);
+			})
+			.then(response => response.json())
+			.then(data => {
+				this.setState({
+					users: data.results,
+				});
+			})
+			.catch(error => console.log(error));
 	};
-
-	componentDidMount() {
-		this.idI = setInterval(this.getData, 1000);
-	}
-
-	componentWillUnmount() {
-		clearInterval(this.idI);
-	}
-
 	render() {
-		const comments = this.state.comments.map(comment => (
-			<div key={comment.id}>
-				<h4>{comment.title}</h4>
-				<h5>{comment.body}</h5>
+		const users = this.state.users;
+		return (
+			<div>
+				<ButtonFetchUsers click={this.handleDataFetch} />
+				{users ? <UsersList users={users} /> : users}
 			</div>
-		));
-
-		return <div className='App'>{comments.reverse()}</div>;
+		);
 	}
 }
 

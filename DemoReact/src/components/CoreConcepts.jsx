d5fea40838c56a9e320350c49f00264a5c87e7cf
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-12-02 21:52:16 +0100

    Code defragmentation
    Fixing the error of redrawing the beginning of the header by useState in the App.js file. Moving useState to Examples.js so that only it gets updated.

---
+++ DemoReact/src/components/CoreConcepts.jsx
@@ -0,0 +1,19 @@
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+
+export default function CoreCOncepts() {
+	return (
+		<section id='core-concepts'>
+			<h2>Core Concepts</h2>
+			<ul>
+				{CORE_CONCEPTS.map(conceptItem => (
+					<CoreConcept key={conceptItem.title} {...conceptItem} />
+				))}
+				{/* <CoreConcept {...CORE_CONCEPTS[0]} />
+						<CoreConcept {...CORE_CONCEPTS[3]} />
+						<CoreConcept {...CORE_CONCEPTS[1]} />
+						<CoreConcept {...CORE_CONCEPTS[2]} /> */}
+			</ul>
+		</section>
+	);
+}

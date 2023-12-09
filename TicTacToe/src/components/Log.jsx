623b9f4ccced27e4fe4351b1faae6969715623e2
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-12-09 14:33:52 +0100

    Update Log.jsx

--- TicTacToe/src/components/Log.jsx
+++ TicTacToe/src/components/Log.jsx
@@ -1,8 +1,11 @@
-export default function Log({turns}){
-    const log = turns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected: {turn.square.row} - {turn.square.col}</li>)
-    return(
-        <ol id="log">
-            {log}
-        </ol>
-    )
+export default function Log({ turns }) {
+	return (
+		<ol id='log'>
+			{turns.map(turn => (
+				<li key={`${turn.square.row}${turn.square.col}`}>
+					{turn.player} selected: {turn.square.row} - {turn.square.col}
+				</li>
+			))}
+		</ol>
+	);
 }

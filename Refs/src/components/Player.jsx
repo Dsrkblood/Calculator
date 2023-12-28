540118645457f7cecb3afeccd296426b5a063d96
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2023-12-28 15:51:40 +0100

    Update Player.jsx

--- Refs/src/components/Player.jsx
+++ Refs/src/components/Player.jsx
@@ -5,17 +5,15 @@ export default function Player() {
 	const [enteredPlayerName, setEnteredPlayerName] = useState();
 
 	function handleClick() {
-		setEnteredPlayerName(playerName.current.value)
+		setEnteredPlayerName(playerName.current.value);
+		playerName.current.value = "";
 	}
 
 	return (
 		<section id='player'>
 			<h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
 			<p>
-				<input
-					ref={playerName}
-					type='text'
-				/>
+				<input ref={playerName} type='text' />
 				<button onClick={handleClick}>Set Name</button>
 			</p>
 		</section>

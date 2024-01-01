68af8383602e6ad9de46176d9bf576e81a68927c
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-01-01 15:52:52 +0100

    Modal changes

--- Refs/src/components/TimerChallenge.jsx
+++ Refs/src/components/TimerChallenge.jsx
@@ -5,33 +5,34 @@ export default function TimerChallenge({ title, targetTime }) {
 	const timer = useRef();
 	const dialog = useRef();
 
-	// const [timerStarted, setTimerStarted] = useState(false);
-	// const [timerExpired, setTimerExpired] = useState(false);
 	const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 	const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
 	if (timeRemaining <= 0) {
 		clearInterval(timer.current);
 		setTimeRemaining(targetTime * 1000);
-		dialog.current.open()
+		dialog.current.open();
 	}
 
 	function handleStart() {
 		timer.current = setInterval(() => {
-			// setTimerExpired(true);
-			// dialog.current.open();
+
 			setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
 		}, 10);
 	}
 
 	function handleStop() {
-		dialog.current.open()
+		dialog.current.open();
 		clearInterval(timer.current);
 	}
 
 	return (
 		<>
-			<ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+			<ResultModal
+				ref={dialog}
+				targetTime={targetTime}
+				remainingTime={timeRemaining}
+			/>
 
 			<section className='challenge'>
 				<h2>{title}</h2>

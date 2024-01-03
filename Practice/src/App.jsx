ce7d1a09efa2e0a2d5531b9a29f41062ddb43816
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-01-03 21:55:59 +0100

    Adding tasks to projects

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -8,7 +8,26 @@ function App() {
 	const [projectsState, setProjectsState] = useState({
 		selectedProjectId: undefined,
 		projects: [],
+		tasks: [],
 	});
+
+	function handleAddTask(text) {
+		setProjectsState(prevState => {
+			const taskId = Math.random();
+			const newTask = {
+				text: text,
+				projectId: prevState.selectedProjectId,
+				id: taskId
+			};
+			return {
+				...prevState,
+				tasks: [newTask,...prevState.tasks]
+
+			};
+		});
+	}
+
+	function handleDeleteTask() {}
 
 	function handleDeleteProject() {
 		setProjectsState(prevState => {
@@ -69,7 +88,13 @@ function App() {
 	);
 
 	let content = (
-		<SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+		<SelectedProject
+			project={selectedProject}
+			onDelete={handleDeleteProject}
+			onAddTask={handleAddTask}
+			onDeleteTask={handleDeleteTask}
+			tasks={projectsState.tasks}
+		/>
 	);
 
 	if (projectsState.selectedProjectId === null) {

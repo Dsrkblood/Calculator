1a6ae6704bf57a54e1fa2a10e9526d8d0406b666
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-01-04 11:33:05 +0100

    Display modal in case of invalid validation of added task.
    When adding tasks, a validation of the shuffle is carried out to check if we accidentally add an empty task. If the event is registered as an invalid value, the Modal will be displayed.

--- Practice/src/components/NewTask.jsx
+++ Practice/src/components/NewTask.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Modal from "./Modal";
 
 export default function NewTask({ onAdd }) {
+	const modal = useRef();
 	const [eneteredTask, setEnteredTask] = useState("");
 
 	function handleChange(event) {
@@ -9,14 +11,28 @@ export default function NewTask({ onAdd }) {
 
 	function handleClick() {
 		if (eneteredTask.trim() === "") {
+			modal.current.open();
 			return;
 		}
 		onAdd(eneteredTask);
 		setEnteredTask("");
 	}
 
+    
+    }
+
 	return (
 		<div className='flex items-center gap-4'>
+			<Modal ref={modal} buttonCaption='Close'>
+				<h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+				<p className='text-stone-600 mb-4'>
+					Ooops... you can't add an empty task
+				</p>
+				<p className='text-stone-600 mb-4'>
+					{" "}
+					Please enter the task in the input field.
+				</p>
+			</Modal>
 			<input
 				type='text'
 				className='w-64 px-2 py-1 rounded-sm bg-stone-200'

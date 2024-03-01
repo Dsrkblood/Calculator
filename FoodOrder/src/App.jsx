5d2751e5f87272884477fcbb1980835685367f0d
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-03-01 14:53:52 +0100

    Create a Checkout component as a Modal containing a delivery data form. Removing the error of closing the <dialog> window by ESC.

--- FoodOrder/src/App.jsx
+++ FoodOrder/src/App.jsx
@@ -1,4 +1,5 @@
 import Cart from "./components/Cart/Cart.jsx";
+import Checkout from "./components/Cart/Checkout.jsx";
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals/Meals.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
@@ -11,6 +12,7 @@ function App() {
 				<Header />
 				<Meals />
 				<Cart />
+				<Checkout />
 			</CartContextProvider>
 		</UserProgressContextProvider>
 	);
---
+++ FoodOrder/src/components/Cart/Checkout.jsx
@@ -0,0 +1,53 @@
+import { useContext } from "react";
+
+import Modal from "../../UI/Modal.jsx";
+import CartContext from "../../store/CartContext.jsx";
+import { currencyFormatter } from "../../util/formatting.js";
+import Input from "../../UI/Input.jsx";
+import Button from "../../UI/Button.jsx";
+import UserProgressContext from "../../store/UserProgressContext.jsx";
+
+export default function Checkout() {
+	const cartCtx = useContext(CartContext);
+	const userProgressCtx = useContext(UserProgressContext);
+
+	const cartTotalPrice = cartCtx.items.reduce((totalPrice, item) => {
+		return totalPrice + item.quantity * item.price;
+	}, 0);
+
+	function handleClose() {
+		userProgressCtx.hideCheckout();
+	}
+
+	function handleSubmit(event) {
+        event.preventDefault();
+
+        
+    }
+
+	return (
+		<Modal
+			open={userProgressCtx.progress === "checkout"}
+			onClose={handleClose}
+			onSubmit={handleSubmit}>
+			<form>
+				<h2>Checkout</h2>
+				<p>Total Amount: {currencyFormatter.format(cartTotalPrice)}</p>
+
+				<Input label='Full Name' type='text' id='full-name' />
+				<Input label='E-Mail Address' type='email' id='email' />
+				<Input label='Street' type='text' id='street' />
+				<div className='control-row'>
+					<Input label='Postal Code' type='text' id='postal-code' />
+					<Input label='City' type='text' id='city' />
+				</div>
+				<p className='modal-actions'>
+					<Button textOnly type='button' onClick={handleClose}>
+						Close
+					</Button>
+					<Button>Submit Order</Button>
+				</p>
+			</form>
+		</Modal>
+	);
+}

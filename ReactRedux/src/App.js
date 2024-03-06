3c7e9fc42216cb4a4c45ea5fa89390c578e36c0c
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-03-06 16:47:30 +0100

    Learning Redux Toolkit

--- ReactRedux/src/App.js
+++ ReactRedux/src/App.js
@@ -1,10 +1,20 @@
-import Counter from './components/Counter';
+import Counter from "./components/Counter";
+import Header from "./components/Header.js";
+import Auth from "./components/Auth.js";
+import UserProfile from "./components/UserProfile.js";
 
+import { useSelector } from "react-redux";
 
 function App() {
-  return (
-    <Counter />
-  );
+	const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+	return (
+		<>
+			<Header />
+			{isAuth ? <UserProfile /> : <Auth />}
+			<Counter />
+		</>
+	);
 }
 
 export default App;

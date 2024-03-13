daaf85ebedcc597d23ba04700e010220cd73f12e
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-03-13 13:34:04 +0100

    Introduction to react router

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
+function App() {
+	return <RouterProvider router={router} />;
+}
+
+export default App;

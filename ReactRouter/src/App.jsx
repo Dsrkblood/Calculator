eda6672c1d465aaff8a33f64759075942192020a
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-03-13 14:23:26 +0100

    Own error message, non-existent page

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import Products from "./pages/Products.jsx";
 import RootLayout from "./pages/RootLayout.jsx";
+import Error from "./pages/Error.jsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <RootLayout />,
+		errorElement: <Error />,
 		children: [
 			{ path: "/", element: <HomePage /> },
 			{ path: "/products", element: <Products /> },

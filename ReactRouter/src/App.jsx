7c33d7ab4b69232d1078cedbd8d0fb479e4948d4
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-03-13 14:05:05 +0100

    path can have children to whom it transmits, for example, the headline. Adding module.css styling

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import Products from "./pages/Products.jsx";
+import RootLayout from "./pages/RootLayout.jsx";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <HomePage /> },
-	{ path: "/products", element: <Products /> },
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ path: "/", element: <HomePage /> },
+			{ path: "/products", element: <Products /> },
+		],
+	},
 ]);
 
 function App() {

c18e8fd3485d68524f27a2525fed0d0462a0582e
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-03-19 12:38:56 +0100

    Automatically log out after 1h

--- Authentication/frontend/src/App.js
+++ Authentication/frontend/src/App.js
@@ -13,13 +13,19 @@ import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+	action as authAction,
+} from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
+import { checkAuthLoader, tokenLoader } from "./util/auth.js";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <RootLayout />,
 		errorElement: <ErrorPage />,
+		id: "root",
+		loader: tokenLoader,
 		children: [
 			{ index: true, element: <HomePage /> },
 			{
@@ -45,6 +51,7 @@ const router = createBrowserRouter([
 								path: "edit",
 								element: <EditEventPage />,
 								action: manipulateEventAction,
+								loader: checkAuthLoader,
 							},
 						],
 					},
@@ -52,6 +59,7 @@ const router = createBrowserRouter([
 						path: "new",
 						element: <NewEventPage />,
 						action: manipulateEventAction,
+						loader: checkAuthLoader,
 					},
 				],
 			},
@@ -60,7 +68,8 @@ const router = createBrowserRouter([
 				element: <NewsletterPage />,
 				action: newsletterAction,
 			},
-			{ path: "auth", element: <AuthenticationPage /> },
+			{ path: "auth", element: <AuthenticationPage />, action: authAction },
+			{ path: "logout", action: logoutAction },
 		],
 	},
 ]);

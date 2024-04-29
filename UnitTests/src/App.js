6b37c3b694710e4b867ba947670369babeed420b
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-04-29 20:23:14 +0200

    Testing asynchronous data

--- UnitTests/src/App.js
+++ UnitTests/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Greating from "./components/Greeting";
+import Async from "./components/Async";
 
 function App() {
 	return (
 		<div className='App'>
 			<Greating />
+			<Async />
 		</div>
 	);
 }
